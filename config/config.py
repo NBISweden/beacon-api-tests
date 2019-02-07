9bb61a1957db2cde3346a153e3fdf8fdd2705d7a
MalinAhlberg <malin.ahlberg@nbis.se>
2019-02-07 16:38:39 +0100

    Add settings for precision, schemas and 0/1 basedness

--- config/config.py
+++ config/config.py
@@ -1,6 +1,8 @@
 
-
+# Path to the OpenAPI spec
 SPEC = 'beacon.yaml'
+
+# Lists of hosts to test
 HOSTS = {
     'local': 'http://localhost:5050',
     # sv beacon must end with '/'
@@ -8,3 +10,12 @@ HOSTS = {
     'es': 'https://ega-archive.org/beacon-api',
     'fi': 'https://beaconpy-elixirbeacon.rahtiapp.fi',
     }
+
+# Directory containing JSON schemas
+SCHEMAS = 'schemas'
+
+#  Floats are rounded in comparisions, set the precision (number of digits) here
+PRECISION = 3
+
+# Is the beacon 0 or 1 based?
+START_POS = 1
