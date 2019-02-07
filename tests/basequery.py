6ec477ef5cfd48ba487fde467d6f40347227bc22
MalinAhlberg <malin.ahlberg@nbis.se>
2019-02-07 16:35:20 +0100

    Use testdata in queries

--- tests/basequery.py
+++ tests/basequery.py
@@ -3,12 +3,13 @@
 def base():
     """ Standard question """
     # This must be a function so that it always returns a new object
-    q = {'referenceName': 1,
-         'referenceBases': 'GG',
-         'alternateBases': 'N',
-         'assemblyId': 'GRCh37',
-         'start': 0,
-         'end': 2,
-         'includeDatasetResponses': 'HIT'
-        }
-    return q
+    query = {'referenceName': 22,
+             'referenceBases': 'GG',
+             'alternateBases': 'N',
+             'assemblyId': 'GRCh38',
+             'start': 0,
+             'end': 2,
+             'includeDatasetResponses': 'HIT',
+             'datasetIds': 'GRCh38:beacon_test:2030-01-01'
+            }
+    return query
